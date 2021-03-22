57d864a5040abab3f37bfd8598ae739adc473e7c
ruanthow <ruan.thow3@gmail.com>
2021-03-22 00:58:21 -0300

    CreateUsers Funcionando

--- src/components/UserRegister/index.tsx
+++ src/components/UserRegister/index.tsx
@@ -20,128 +20,133 @@ export function UserRegister() {
         isNotEmail,
         isNotName,
         isNotPassword,
+        setIsCreateLoading,
+        isCreateLoading
 
     } = useContext(UserRegisterContexts);
-
 
 
     return (
 
 
         <div className={styled.userRegisterContainer}>
+            {!isCreateLoading ? 
+            (<div className={styled.userRegisterBox}>
+                <div className={styled.teste}>
+                    <BackArrow href="/" />
+                </div>
+                <div className={styled.userRegisterContents}>
 
-            { !iscreateUser ?
-                (<div className={styled.userRegisterBox}>
-                    <div className={styled.teste}>
-                        <BackArrow href="/" />
-                    </div>
-                    <div className={styled.userRegisterContents}>
+                    <img src="logo.svg" alt="" />
+                    <p>Bem vindo</p>
+                    <form onSubmit={(e) => {
+                        ValidationAll();
+                        if (iscreateUser == true) {
+
+                            console.log(name, email, password);
+                            setIsCreateLoading(true)
+                            CreateUser();
+
+                            setTimeout(() => {
+                                
+                            }, 2000);
+                        }
+
                         
-                        <img src="logo.svg" alt="" />
-                        <p>Bem vindo</p>
-                        <form onSubmit={(e) => {
-                            ValidationAll();
-                            e.preventDefault()
-                            if (iscreateUser == true) {
+                    }}>
+                        <div className={isNotName ? styled.userRegisterInput : styled.userRegisterInputInvalid}>
+                            <input
 
-                                console.log("Create success");
+                                type="text"
+                                name="name"
+                                placeholder="Digite seu Nome"
+                                required
+                                onChange={
+                                    (e) => {
+                                        setName((e.target.value).replace(/[0-9]/g, ''))
+                                    }
+                                } />
 
-                            }
+                            {!isNotName && <span>Use apenas caracteres no minimo 4</span>}
+                        </div>
+                        <div className={isNotEmail ? styled.userRegisterInput : styled.userRegisterInputInvalid}>
+                            <input
 
-                            //CreateUser();
-                        }}>
-                            <div className={isNotName ? styled.userRegisterInput : styled.userRegisterInputInvalid}>
-                                <input
+                                name="email"
+                                placeholder="Digite e-mail"
+                                required
+                                title="Exemplo exemplo@exemplo.com"
+                                onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }} />
+                            {!isNotEmail && <span>Ultilize um email valido exemplo@exemplo.com</span>}
+                        </div>
+                        <div className={isNotPassword ? styled.userRegisterInput : styled.userRegisterInputInvalid}>
+                            <input
 
-                                    type="text"
-                                    name="name"
-                                    placeholder="Digite seu Nome"
-                                    required
-                                    onChange={
-                                        (e) => {
-                                            setName((e.target.value).replace(/[0-9]/g, ''))
-                                        }
-                                    } />
+                                type="password"
+                                name="password"
+                                placeholder="Digite sua senha"
+                                required
+                                title="Minimo 6 digitos"
+                                onChange={(e) => {
+                                    setPassword(e.target.value)
+                                }} />
+                            {!isNotPassword && <span>No mínimo 6 caracteres maxímo 15</span>}
+                        </div>
+                        <div className={password != passwordConfirm ? styled.userRegisterInputInvalid : styled.userRegisterInput}>
+                            <input
 
-                                {!isNotName && <span>Use apenas caracteres no minimo 4</span>}
-                            </div>
-                            <div className={isNotEmail ? styled.userRegisterInput : styled.userRegisterInputInvalid}>
-                                <input
+                                type="password"
+                                name="passwordConfirm"
+                                placeholder="Confirmar sua senha"
+                                required
+                                title="Diferente da senha digitada acima"
+                                onChange={(e) => {
+                                    setPasswordConfirm(e.target.value)
+                                }} />
+                            {password != passwordConfirm && <span>Não condiz com o campo anterior</span>}
+                        </div>
 
-                                    name="email"
-                                    placeholder="Digite e-mail"
-                                    required
-                                    title="Exemplo exemplo@exemplo.com"
-                                    onChange={(e) => {
-                                        setEmail(e.target.value)
-                                    }} />
-                                {!isNotEmail && <span>Ultilize um email valido exemplo@exemplo.com</span>}
-                            </div>
-                            <div className={isNotPassword ? styled.userRegisterInput : styled.userRegisterInputInvalid}>
-                                <input
+                        <button
 
-                                    type="password"
-                                    name="password"
-                                    placeholder="Digite sua senha"
-                                    required
-                                    title="Minimo 6 digitos"
-                                    onChange={(e) => {
-                                        setPassword(e.target.value)
-                                    }} />
-                                {!isNotPassword && <span>No mínimo 6 caracteres</span>}
-                            </div>
-                            <div className={password != passwordConfirm ? styled.userRegisterInputInvalid : styled.userRegisterInput}>
-                                <input
-
-                                    type="password"
-                                    name="passwordConfirm"
-                                    placeholder="Confirmar sua senha"
-                                    required
-                                    title="Diferente da senha digitada acima"
-                                    onChange={(e) => {
-                                        setPasswordConfirm(e.target.value)
-                                    }} />
-                                {password != passwordConfirm && <span>Não condiz com o campo anterior</span>}
-                            </div>
-
-                            <button
-
-                                type="submit"
-                                onClick={Security}
-                            >
-                                Continuar
+                            type="submit"
+                            onClick={Security}
+                        >
+                            Continuar
                     </button>
 
-                        </form>
-                        <div className={styled.userRegisterOthersLogins}>
-                            <img
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
-                                alt="" />
-                            <img src="https://cdn.iconscout.com/icon/free/png-512/facebook-logo-2019-1597680-1350125.png" alt="" />
-                            <img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" alt="" />
-                        </div>
-
+                    </form>
+                    <div className={styled.userRegisterOthersLogins}>
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
+                            alt="" />
+                        <img src="https://cdn.iconscout.com/icon/free/png-512/facebook-logo-2019-1597680-1350125.png" alt="" />
+                        <img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" alt="" />
                     </div>
-                </div>)
 
-                :
-                (<div className={styled.loadingRegisterBox}>
-                    <div className={styled.loadingRegisterContainte}>
-
-                        <div className={styled.svgLoading}>
-                            <svg version="1.1" id="L9" height="350px" width="350px"
-                                viewBox="0 0 100 100">
-                                <path fill="#ff0707" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-
-                                </path>
-                            </svg>
-                        </div>
-                    Cadastrando...
                 </div>
-                </div>)
+            </div>)
+
+
+                : (
+
+                    <div className={styled.loadingRegisterBox}>
+                        <div className={styled.loadingRegisterContainte}>
+
+                            <div className={styled.svgLoading}>
+                                <svg version="1.1" id="L9" height="350px" width="350px"
+                                    viewBox="0 0 100 100">
+                                    <path fill="#ff0707" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+
+                                    </path>
+                                </svg>
+                            </div>
+                                Cadastrando...
+                        </div>
+                    </div>
+                )
             }
-
-
         </div>
 
 
--- src/contexts/UserLoginContexts.tsx
+++ src/contexts/UserLoginContexts.tsx
@@ -5,120 +5,134 @@ import { GetServerSideProps } from "next";
 import { stringify } from "node:querystring";
 
 
-interface UserLoginData{
-  user:string;
-  setUser:any;
-  password:string;
-  setPassword:any;
-  Logando:Function;
-  DBLOGIN:Function;
-  setIsLoading:any;
-  isLoading:boolean;
-  nameUser:string;
-  error:boolean;
-  
+interface UserLoginData {
+    user: string;
+    setUser: any;
 
-  
+    password: string;
+    setPassword: any;
+
+
+    Logando: Function;
+    DBLOGIN: Function;
+
+    setIsLoading: any;
+    isLoading: boolean;
+    nameUser: string;
+    setNameUser:any;
+    error: boolean;
+
+
+
 
 }
-interface UserLoginProviderProps{
-    children : ReactNode;
-    validationLoginIndex:any;
-   
-    
+interface UserLoginProviderProps {
+    children: ReactNode;
+    validationLoginIndex: any;
+
+
 }
 
 export const UserLoginContexts = createContext({} as UserLoginData);
 
-export  function UserLoginProvider({children, ...rest}: UserLoginProviderProps){
+export function UserLoginProvider({ children, ...rest }: UserLoginProviderProps) {
 
-    
+
     const [user, setUser] = useState();
     const [password, setPassword] = useState();
     const [isLoading, setIsLoading] = useState(false);
     const [nameUser, setNameUser] = useState();
     const [error, setError] = useState(false);
-    
-    
 
-    if(Cookies.get('Token') != undefined){
+
+
+    if (Cookies.get('Token') != undefined) {
         rest.validationLoginIndex(true);
     }
-    
 
-    async function DBLOGIN() { 
-         
-        const body = {
-        "email":user,
-        "pwd":password,      
+   
+        async function teste() {
+
+            const data = await fetch("https://fitbodyapi.herokuapp.com/users", { method: "GET", headers: { 'Content-Type': 'application/json', 'authorization': `Bearer ${Cookies.get("Token")}` } })
+            if (data.status == 200) {
+                rest.validationLoginIndex(true);
+            }
+            else if (data.status != 200) {
+
+                rest.validationLoginIndex(false);
+            }
+            
+            const UserData = await data.json();
+            setNameUser(UserData.name)
+            console.log(UserData);
         }
-               
+     
+    useEffect(() => {
+        teste()
+    }, []);
+
+    
+    async function DBLOGIN() {
+
+        const body = {
+            "email": user,
+            "pwd": password,
+        }
+
         const USER = await fetch("https://fitbodyapi.herokuapp.com/auth",
-        {method:"POST",headers:{'Content-Type': 'application/json'},  body:JSON.stringify(body)})
+            { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) })
         Logando(USER.status);
-        
+
         const data = await USER.json();
         Cookies.set('Token', data.token);
-        
-    }   
-      
+        console.log(data);
+        teste();
+    }
 
 
-    useEffect(() => {
-    const data = fetch("https://fitbodyapi.herokuapp.com/users",{method:"GET",headers:{'Content-Type': 'application/json','authorization':`Bearer ${Cookies.get("Token")}`}})
-     .then((data)=>{
-         
-       if(data.status == 200){
-        rest.validationLoginIndex(true);
-       }
-       else if(data.status != 200){
-        
-        rest.validationLoginIndex(false);
-       }
-    }).catch((e)=>{
-       
-    })      
-    }, []);
+
     
-    function Logando(status:number) {
-        
-        if(status == 200 ){
-         rest.validationLoginIndex(true);
-         setIsLoading(false);
-         setError(false);
+
+    function Logando(status: number) {
+
+        if (status == 200) {
+            rest.validationLoginIndex(true);
+            setIsLoading(false);
+            setError(false);
 
         }
-        else if(status != 200){
-         setIsLoading(false);
-         setError(true);
+        else if (status != 200) {
+            setIsLoading(false);
+            setError(true);
         }
     }
-    
-   
-          
-    
-       
-    
-    return(
-        <UserLoginContexts.Provider 
-        value={{
-            user,
-            setUser,
-            password,
-            setPassword,
-            Logando,
-            DBLOGIN,
-            setIsLoading,
-            isLoading,
-            nameUser,
-            error
-         
-            
+
+
+
+
+
+
+    return (
+        <UserLoginContexts.Provider
+            value={{
+                user,
+                setUser,
+                password,
+                setPassword,
+                Logando,
+                DBLOGIN,
+                setIsLoading,
+                isLoading,
+                nameUser,
+                error,
+                setNameUser
+
+
             }}
-            >
+        >
 
-              {children}
+            {children}
 
-        </UserLoginContexts.Provider>       
+        </UserLoginContexts.Provider>
     )
 }
