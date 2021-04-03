a95d0f874f04d9563908624d2490e79fbd7af8a9
ruanthow <ruan.thow3@gmail.com>
2021-04-03 20:18:53 -0300

    Perfil Atualizado

--- src/components/EditProfile/index.tsx
+++ src/components/EditProfile/index.tsx
@@ -1,6 +1,6 @@
 import styled from '../../styles/components/EditProfile.module.css'
 import { motion } from 'framer-motion';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { EditContexts } from '../../contexts/UserEditContexts';
 import Cookies from 'js-cookie';
 
@@ -9,25 +9,41 @@ import Cookies from 'js-cookie';
 export default function EditProfile() {
 
     const { setIsEdit, isEdit, uploadPhoto, setUploadPhoto, setSendPhoto, sendPhoto, ChangeImage } = useContext(EditContexts);
+    const [preview, setPreview] = useState(null);
+    const [isNotPhoto, setIsNotPhoto] = useState(false);
+    const [chooseImage, setChooseimage] = useState(false);
 
+
+    async function CheckImage(photo) {
+
+        if (photo == "application/x-msdownload") {
+            setIsNotPhoto(true);
+        }
+        else {
+            setIsNotPhoto(false);
+            setSendPhoto(photo);
+        }
+    }
     useEffect(() => {
-       console.log(sendPhoto)
+        console.log(sendPhoto)
     }, [sendPhoto]);
+
 
     return (
         <div className={styled.Overlay}>
             <motion.div
-            initial={{x:-1500}}
-            animate={{
-                x:0,
-                transition:{
-                    stiffness: 50,
-                    delay: 0.7,
-                    type: "spring",
-                    bounce:3000
-                }
-            }}
-            className={styled.Container}
+                className={styled.Container}
+                initial={{ x: -1500 }}
+                animate={{
+                    x: 0,
+                    transition: {
+                        stiffness: 50,
+                        delay: 0.7,
+                        type: "spring",
+                        bounce: 3000
+                    }
+                }}
+
             >
                 <motion.div
                     whileHover={{ scale: 1.1 }}
@@ -40,95 +56,128 @@ export default function EditProfile() {
                         }
                     }}
                 >
-                    <img src="icons/cross.svg" alt="" />  
+                    <img src="icons/cross.svg" alt="" />
                 </motion.div>
-                <div className={styled.Header}>
+            {chooseImage &&
+                <div className={styled.Preview}>
+                    <div className={styled.ChooseImage}>
+                        <label htmlFor="arquivo">
+                            <input accept="image/*" type="file" id="arquivo" onChange={(e) => {
+                                CheckImage(e.target.files.item(0).type);
+                                setPreview(URL.createObjectURL(e.target.files[0]))
+                                // setSendPhoto(e.target.files);
+                                // console.log(e.target.files[0].type);         
+                               
+                            }}
+                            />
+                            Choose image
+                        </label>
+                        
+                    </div>
                     
+                    <div className={styled.PreviewPhoto}>
+                        {!isNotPhoto && <img src={`${preview == null ? "icons/user.svg" : preview} `} alt=""/> || isNotPhoto && 
+                        <div className={styled.ErrorFormatPhoto}>
+                            <span>*Formato não compativel</span>
+                        </div>}
+                    </div>
+
+                    <div className={styled.ButtonArea}>
+                        <motion.div
+                        whileTap={{scale:1.2}}
+                        >
+                            Alterar
+                        </motion.div>
+                        <motion.div
+                        whileTap={{scale:1.2}}
+                        onClick={()=>{
+                            setChooseimage(false);
+                        }}
+                        >
+                            Cancela
+                        </motion.div>
+                    </div>
+                    
+                </div>
+                }
+                
+                <div className={styled.Header}>
                     <div className={styled.Profile}>
                         <div className={styled.Photo}>
-                            <label htmlFor="arquivo">
-                                <input accept="image/*" type="file" id="arquivo" onChange={(e) => {                         
-                                        console.log(e.target.files)
-
-
-                                        // setSendPhoto(e.target.files);
-                                        // console.log(e.target.files[0].type);         
-                                    //ChangeImage();
-                                }}
-                                />
-                                <div className={styled.clickedArea}>
-                                    <img src="https://github.com/ruanthow.png" />
-                                    <div className={styled.overlayPhoto}>
-                                        <img src="icons/camera.svg" alt="" />
-                                    </div>
+                            <div className={styled.clickedArea} onClick={()=>{
+                                setChooseimage(true)
+                            }}>
+                                <img src="https://github.com/ruanthow.png" />
+                                <div className={styled.overlayPhoto}>
+                                    <img src="icons/camera.svg" alt="" />
                                 </div>
-                                </label>
-                        </div>
-
+                            </div>
                         </div>
 
                     </div>
 
-                    <div className={styled.UserName}>
-                        <h2>The Winchester</h2>
+                </div>
+
+                <div className={styled.UserName}>
+                    <h2>The Winchester</h2>
+                </div>
+
+
+                <div className={styled.spanLine}>
+                    <span></span>
+                </div>
+                <div className={styled.Forms}>
+
+                    <div className={styled.BoxInput}>
+                        <div className={styled.Input}>
+                            <h3>Email:</h3>
+                            <p>ruan@hotmail.com</p>
+                            <div style={{ margin: "2rem" }}></div>
+                        </div>
                     </div>
 
-
-                    <div className={styled.spanLine}>
-                        <span></span>
-                    </div>
-                    <div className={styled.Forms}>
-
-                        <div className={styled.BoxInput}>
-                            <div className={styled.Input}>
-                                <h3>Email:</h3>
-                                <p>ruan@hotmail.com</p>
-                                <div style={{ margin: "2rem" }}></div>
-                            </div>
-                        </div>
-
-                        <div className={styled.BoxInput}>
-                            <div className={styled.Input}>
-                                <h3>Name:</h3>
-                                <p>The Whinchester</p>
-                                <motion.div
-                                    whileTap={{ scale: 1.1 }}
-                                    className={styled.Button}
-                                >
-                                    Editar
-                            </motion.div>
-                            </div>
-                        </div>
-
-                        <div className={styled.ChangePassword}>
-                            <div className={styled.Contents}>
-                                <p>*Para modificar sua senha fique atento vamos pedir dados anteriores</p>
-                                <motion.div
-                                    whileTap={{ scale: 1.1 }}
-                                    className={styled.InputPassword}
-                                >
-                                    Mudar Senha
-                            </motion.div>
-                            </div>
-                        </div>
-                        <div className={styled.ExitApp}>
+                    <div className={styled.BoxInput}>
+                        <div className={styled.Input}>
+                            <h3>Name:</h3>
+                            <p>The Whinchester</p>
                             <motion.div
-                            whileTap={{ scale: 1.1 }} 
+                                whileTap={{ scale: 1.1 }}
+                                className={styled.Button}
+                            >
+                                Editar
+                            </motion.div>
+                        </div>
+                    </div>
+
+                    <div className={styled.ChangePassword}>
+                        <div className={styled.Contents}>
+                            <p>*Para modificar sua senha fique atento vamos pedir dados anteriores</p>
+                            <motion.div
+                                whileTap={{ scale: 1.1 }}
+                                className={styled.InputPassword}
+                            >
+                                Mudar Senha
+                            </motion.div>
+                        </div>
+                    </div>
+                    <div className={styled.ExitApp}>
+                        <motion.div
+                            whileTap={{ scale: 1.1 }}
                             className={styled.ButtonExit}
-                            onClick={()=>{
-                                
+                            onClick={() => {
+
                                 Cookies.remove("Token");
                                 Cookies.remove("currentExperience");
                                 Cookies.remove("completeChallenge");
                                 Cookies.remove("level");
                                 window.location.reload();
                             }}
-                            >
-                                 Sair
+                        >
+                            Sair
                             </motion.div>
-                        </div>
                     </div>
-                </motion.div>
-            </div>
+                </div>
+            </motion.div>
+        </div>
     )
 }
---
+++ src/pages/confirm_email/index.tsx
@@ -0,0 +1,12 @@
+
+
+
+
+
+export default function ConfirmEmail(){
+    return(
+        <div>
+            Teste
+        </div>
+    )
+}
